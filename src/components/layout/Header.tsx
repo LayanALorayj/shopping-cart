bf305b92f271570caaf3761fba2a3b0a2fc59fb3
LayanALorayj <layanAlorayj@outlook.com>
2025-10-12 11:31:09 +0300

    fix header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,23 +1,64 @@
 import React from "react";
-import { ShoppingOutlined, ShoppingCartOutlined } from "@ant-design/icons";   
+import { Layout } from 'antd'; 
+import { ShoppingOutlined, ShoppingCartOutlined } from "@ant-design/icons"; 
 import { useCart } from "../../context/CartContext";
-import "../../App.css";
+
+const { Header: AntHeader } = Layout; 
+
+const customAntHeaderStyle: React.CSSProperties = {
+
+  background: 'linear-gradient(90deg, #cda0b2, #bc6789)', 
+  color: 'white',
+  height: 64, 
+  padding: '1rem 2.5rem',
+  display: 'flex',
+  justifyContent: 'space-between', 
+  alignItems: 'center',
+  
+  lineHeight: 'normal', 
+  
+  boxShadow: '0 2px 10px rgba(0, 0, 0, 0.15)',
+  
+  position: 'sticky',
+  top: 0,
+  zIndex: 10,
+};
+
+const shopTitleStyles: React.CSSProperties = {
+    fontSize: '1.6rem',
+    letterSpacing: '1px',
+    fontWeight: 600,
+    margin: 0, 
+    display: 'flex',
+    alignItems: 'center',
+};
+
+const cartIconContainerStyles: React.CSSProperties = {
+    fontSize: '1.2rem',
+    background: 'rgba(255, 255, 255, 0.2)', 
+    padding: '6px 14px',
+    borderRadius: '20px',
+    fontWeight: 500,
+    display: 'flex',
+    alignItems: 'center',
+    gap: '5px'
+};
 
 const Header: React.FC = () => {
-  const { state } = useCart();
+   const { state } = useCart();
 
-  return (
-    <header>
-      <h2 className="shop-title">
-        <ShoppingOutlined style={{ marginRight: "8px" }} />
-        Shop
-      </h2>
-      <div className="cart-icon">
-        <ShoppingCartOutlined style={{ fontSize: "22px", marginRight: "5px" }} />
-        <span>{state.count}</span>
-      </div>
-    </header>
-  );
+ return (
+  <AntHeader style={customAntHeaderStyle}>
+   <h2 style={shopTitleStyles}>
+    <ShoppingOutlined style={{ marginRight: "8px" }} />
+    Shop
+   </h2>
+   <div style={cartIconContainerStyles}>
+    <ShoppingCartOutlined />
+    <span>{state.count}</span>
+   </div>
+  </AntHeader>
+ );
 };
 
 export default Header;
