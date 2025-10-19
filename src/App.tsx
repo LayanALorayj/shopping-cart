f895bef1bd36f369e60b9475f2814a7674b0abd6
LayanALorayj <layanAlorayj@outlook.com>
2025-10-19 14:28:40 +0300

    add lazy load

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
-import React from "react";
+import React, { Suspense, lazy } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/layout/Header";
-import ProductsPage from "./pages/ProductsPage";
-import CartPage from "./pages/CartPage";
-import HomePage from "./pages/HomePage";
-import ContactUs from "./pages/ContactPage";
-import ProductDetailPage from "./pages/ProductDetailPage";
 import "./App.css";
+
+const HomePage = lazy(() => import("./pages/HomePage"));
+const ProductsPage = lazy(() => import("./pages/ProductsPage"));
+const ProductDetailPage = lazy(() => import("./pages/ProductDetailPage"));
+const CartPage = lazy(() => import("./pages/CartPage"));
+const ContactUs = lazy(() => import("./pages/ContactPage"));
 
 const App: React.FC = () => {
   return (
     <Router>
       <Header />
-      <Routes>
-        <Route path="/" element={<HomePage />} />      
-        <Route path="/products/:category" element={<ProductsPage />} /> 
-        <Route path="/cart" element={<CartPage />} /> 
-         <Route path="/contact" element={<ContactUs />} />  
-         <Route path="/product/:id" element={<ProductDetailPage />} />
-      </Routes>
+      <Suspense
+        fallback={
+          <div style={{ textAlign: "center", marginTop: "100px", fontSize: "18px" }}>
+            ⏳ Loading...
+          </div>
+        }
+      >
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/products/:category" element={<ProductsPage />} />
+          <Route path="/product/:id" element={<ProductDetailPage />} />
+          <Route path="/cart" element={<CartPage />} />
+          <Route path="/contact" element={<ContactUs />} />
+        </Routes>
+      </Suspense>
     </Router>
   );
 };
